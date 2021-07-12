c026f27e59d61ddbdcc9f60a58ed90f501af6153
Ellen Wang <Ellen.Wang@ibm.com>
2021-07-12 13:39:03 -0400

    .

--- scripts/curl_test.sh
+++ scripts/curl_test.sh
@@ -3,10 +3,10 @@
 echo "Starting Node-RED server..."
 nohup bash -c "npm start 2>&1 &" && sleep 5
 echo "Checking '/' page..."
-response1=$(curl --write-out '%{http_code}' --silent --output /dev/null "http://localhost:1880/")
+response1=$(curl --write-out '%{http_code}' --silent --output /dev/null "http://localhost:8080/")
 echo "HTTP Response: $response1"
 if [[ $response1 = 200 ]]; then echo "Success"; else echo "Failed" && exit 1; fi
 echo "Checking '/health' page..."
-response2=$(curl --write-out '%{http_code}' --silent --output /dev/null "http://localhost:1880/health")
+response2=$(curl --write-out '%{http_code}' --silent --output /dev/null "http://localhost:8080/health")
 echo "HTTP Response: $response2"
 if [[ $response2 = 200 ]]; then echo "Success"; else echo "Failed" && exit 1; fi
